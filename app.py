e8889b575d166be2fd033c735c6946b5ef5086a0
postive pillow <postivepillow123@gmail.com>
2024-02-20 16:39:11 +0530

    The Web app is ready

--- app.py
+++ app.py
@@ -24,6 +24,14 @@ def predict_api():
     output=model.predict(new_transformed_data)
     print(output[0])
     return jsonify(output[0])
+@app.route('/predict',methods=['POST'])
+
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_output=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_output)
+    output=model.predict(final_output)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
 
 if __name__=="__main__":
